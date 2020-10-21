530858d1264ee2e7d775b8decfa5ed0ea124d121
whc <181850154@smail.nju.edu.cn>
2020-10-21 17:34:50 +0800

    create: web_crawler

---
+++ parameter.py
@@ -0,0 +1,7 @@
+start_url = 'https://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1&max_behot_time='
+url = 'https://www.toutiao.com'
+
+headers={
+	'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+}
+cookies = {'tt_webid':'6649949084894053895'}
